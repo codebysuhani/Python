48caed8a21ff3d01245f7419c483c8ab2da14525
buildwithsuhani <suhani.online45@gmail.com>
2026-06-27 22:07:53 +0530

    commit_4

---
+++ functions and recursion/avg_func.py
@@ -0,0 +1,10 @@
+#average opf 3 numbers
+
+def calc_avg(a, b, c):
+    avg = (a + b + c)/3
+    print("average of three numbers =", avg)
+    return avg
+
+calc_avg(2, 3, 1)
+
+calc_avg(97, 98, 95)
---
+++ functions and recursion/func.py
@@ -0,0 +1,29 @@
+#function defination
+def calc_sum(a, b):  #parameter
+    sum = a + b
+    print(sum)
+    return sum
+
+calc_sum(5, 10)   #function call; arguments
+
+calc_sum(2, 10)
+
+calc_sum(12, 17)
+
+
+
+def calc_subs(a, b):
+    return a - b
+
+subs = calc_subs(1345, 234)
+print(subs)
+
+
+def print_hello():
+    print("hello SUHANI")
+
+print_hello()
+print_hello()
+print_hello()
+print_hello()
+print_hello()
