26b24eb7e5b0d80eb1cb89558422a481da19ee87
buildwithsuhani <suhani.online45@gmail.com>
2026-07-04 22:01:04 +0530

    commit_8

---
+++ oops/oops.py
@@ -0,0 +1,17 @@
+class Student:
+    name = "suhani"
+
+s1 = Student()
+print(s1)
+print(s1.name)
+
+s2 = Student()
+print(s2.name)
+
+class Car:
+    color = "black"
+    brand = "mercedes"
+
+car1 = Car()
+print(car1.color)
+print(car1.brand)
